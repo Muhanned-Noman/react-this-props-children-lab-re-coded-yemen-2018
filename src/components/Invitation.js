16f1c259ace8d82905b92b6cbd7681a354134419
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 13:49:33 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,17 +1,32 @@
 // Code Invitation Component Here
 
+<<<<<<< HEAD
 import React from 'react';
 // import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 // import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+=======
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+>>>>>>> 6d38b14d166ff4822d43dc83dbf600dad60e575f
 
 
 export default class Invitation  extends React.Component {
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <h1>You've been invited!</h1>
         {this.props.children}
+=======
+        <h1>You\'ve been invited!</h1>
+        <div>{this.props.children}</div>
+>>>>>>> 6d38b14d166ff4822d43dc83dbf600dad60e575f
       </div>
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+export default Invitation;
+>>>>>>> 6d38b14d166ff4822d43dc83dbf600dad60e575f
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,8 +1,13 @@
 // Code ThemedDecoration Component Here
 
+<<<<<<< HEAD
 import React from 'react';
 // import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 // import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+=======
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+>>>>>>> 6d38b14d166ff4822d43dc83dbf600dad60e575f
 
 
 export default class ThemedDecorations  extends React.Component {
@@ -17,3 +22,8 @@ export default class ThemedDecorations  extends React.Component {
     );
   }
 }
+<<<<<<< HEAD
+=======
+
+export default ThemedDecorations;
+>>>>>>> 6d38b14d166ff4822d43dc83dbf600dad60e575f
